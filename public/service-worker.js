18c1f6d5d357beae614c46c84919d744101b99d9
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-12-04 10:18:30 -0800

    Deploying

--- public/service-worker.js
+++ public/service-worker.js
@@ -24,7 +24,6 @@ self.addEventListener('install', function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             console.log('Your files were pre-cached successfully!');
-            console.log(FILES_TO_CACHE);
             return cache.addAll(FILES_TO_CACHE);
         })
     );
